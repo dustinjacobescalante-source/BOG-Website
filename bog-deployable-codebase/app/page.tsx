a8b946df409d373ece79800f1f9ae2c9c5b66452
dustinjacobescalante-source <dustinjacobescalante@gmail.com>
2026-03-17 13:21:24 -0500

    Update page.tsx

--- bog-deployable-codebase/app/page.tsx
+++ bog-deployable-codebase/app/page.tsx
@@ -25,7 +25,7 @@ export default function HomePage() {
             fill
             className="object-contain opacity-60"
           />
-          <div className="absolute inset-0 bg-gradient-to-b from-black/35 via-black/45 to-black" />
+          <div className="absolute inset-0 bg-gradient-to-b from-black/5 via-black/10 to-black" />
         </div>
 
         <div className="relative mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-24">
