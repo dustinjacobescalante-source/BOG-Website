2a945751a1ee6e0d674dd3119856b6c281380242
dustinjacobescalante-source <dustinjacobescalante@gmail.com>
2026-04-21 16:25:10 -0500

    Update LiveMeetingRoom.tsx

--- bog-deployable-codebase/components/meetings/LiveMeetingRoom.tsx
+++ bog-deployable-codebase/components/meetings/LiveMeetingRoom.tsx
@@ -1,1 +1,40 @@
+'use client';
 
+import { LiveKitRoom, VideoConference, RoomAudioRenderer } from '@livekit/components-react';
+import { useRouter } from 'next/navigation';
+
+export default function LiveMeetingRoom({ tokenData }: any) {
+  const router = useRouter();
+
+  if (!tokenData?.token || !tokenData?.url) {
+    return (
+      <div className="p-6 text-center text-red-400">
+        Failed to load meeting connection.
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-3 sm:p-4">
+      <div className="overflow-hidden rounded-[24px] border border-white/10 bg-black">
+        
+        <LiveKitRoom
+          token={tokenData.token}
+          serverUrl={tokenData.url}
+          connect={true}
+          video
+          audio
+          data-lk-theme="default"
+          className="h-[70vh] min-h-[560px] w-full"
+          onDisconnected={() => {
+            router.push('/portal/meetings');
+          }}
+        >
+          <VideoConference />
+          <RoomAudioRenderer />
+        </LiveKitRoom>
+
+      </div>
+    </div>
+  );
+}
