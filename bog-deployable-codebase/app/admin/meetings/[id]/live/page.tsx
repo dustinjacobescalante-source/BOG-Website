22415b5aab5ec3ddd976a071cd6e7bc813966971
dustinjacobescalante-source <dustinjacobescalante@gmail.com>
2026-04-22 12:03:36 -0500

    Update page.tsx

--- bog-deployable-codebase/app/admin/meetings/[id]/live/page.tsx
+++ bog-deployable-codebase/app/admin/meetings/[id]/live/page.tsx
@@ -65,7 +65,6 @@ export default async function AdminLiveMeetingPage({
        <div className="rounded-2xl border border-white/10 bg-black/30 p-4">
   <LiveMeetingRoom meetingId={meeting.id} />
 </div>
-      </div>
     </AdminPageShell>
   );
 }
